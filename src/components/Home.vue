e5a680459aab8e3b1ebe29d7bd029563f319d075
Preiksas <vpreiks@gmail.com>
2018-11-26 21:42:26 +0200

    init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="hello">
+		<h1>{{ room }}</h1>
+	</div>
+</template>
+
+<script>
+
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    computed: {
+      ...mapGetters({
+        room: 'getRoom'
+      })
+    }
+  }
+</script>
